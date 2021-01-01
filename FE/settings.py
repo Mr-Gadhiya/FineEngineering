34f03cce79c5865fe3fe1e30f36478e37ad8580f
Jay Gadhiya <68724568+Mr-Gadhiya@users.noreply.github.com>
2021-01-01 11:57:13 +0530

    Update settings.py

--- FE/settings.py
+++ FE/settings.py
@@ -86,7 +86,7 @@ if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
            'ENGINE': 'django.db.backends.sqlite3',
-           'NAME': BASE_DIR / 'db.sqlite3',
+           'NAME': 'github_actions',
            'USER': 'postgres',
            'PASSWORD': 'postgres',
            'HOST': '127.0.0.1',
