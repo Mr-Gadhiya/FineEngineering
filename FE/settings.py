a7ea45ded30af0dbb0c6b9100bf602b6055b5638
Jay Gadhiya <68724568+Mr-Gadhiya@users.noreply.github.com>
2021-01-01 11:46:37 +0530

    Update settings.py

--- FE/settings.py
+++ FE/settings.py
@@ -82,7 +82,15 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': BASE_DIR / 'db.sqlite3',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
